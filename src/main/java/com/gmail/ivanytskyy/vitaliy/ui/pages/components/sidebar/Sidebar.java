bc6446c1515a8a8a7a7b354d32d3696e35f6e03d
Vitaliy Ivanytskyy <vitaliy.ivanytskyy@gmail.com>
2023-09-29 15:38:18 +0300

    Added Sidebar class

---
+++ src/main/java/com/gmail/ivanytskyy/vitaliy/ui/pages/components/sidebar/Sidebar.java
@@ -0,0 +1,42 @@
+package com.gmail.ivanytskyy.vitaliy.ui.pages.components.sidebar;
+
+import com.gmail.ivanytskyy.vitaliy.ui.pages.*;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * @author Vitaliy Ivanytskyy
+ * @version 1.00
+ * @date 29/09/2023
+ */
+public abstract class Sidebar extends BasePage {
+    @FindBy(xpath = ".//a[contains(@href, '/garage')]")
+    private WebElement garageLink;
+    @FindBy(xpath = ".//a[contains(@href, '/expenses')]")
+    private WebElement fuelExpensesLink;
+    @FindBy(xpath = ".//a[contains(@href, '/instructions')]")
+    private WebElement instructionsLink;
+    @FindBy(css = ".btn.btn-link.text-danger.btn-sidebar.sidebar_btn")
+    private WebElement logoutLink;
+
+    public Sidebar(WebElement element) {
+        PageFactory.initElements(element, this);
+    }
+    public GaragePage openGarage(){
+        clickLink(garageLink);
+        return new GaragePage();
+    }
+    public InstructionsPage openInstructions(){
+        clickLink(instructionsLink);
+        return new InstructionsPage();
+    }
+    public ExpensesPage openExpenses(){
+        clickLink(fuelExpensesLink);
+        return new ExpensesPage();
+    }
+    public MainPage logout(){
+        clickLink(logoutLink);
+        return new MainPage();
+    }
+}
--- src/main/java/com/gmail/ivanytskyy/vitaliy/ui/pages/components/sidebar/UserSidebar.java
+++ src/main/java/com/gmail/ivanytskyy/vitaliy/ui/pages/components/sidebar/UserSidebar.java
@@ -7,29 +7,18 @@ import org.openqa.selenium.support.PageFactory;
 
 /**
  * @author Vitaliy Ivanytskyy
- * @version 1.02
- * @date 04/09/2023
+ * @version 1.03
+ * @date 29/09/2023
  */
-public class UserSidebar extends BasePage {
-    @FindBy(xpath = ".//a[contains(@href, '/garage')]")
-    private WebElement garageLink;
-    @FindBy(xpath = ".//a[contains(@href, '/expenses')]")
-    private WebElement fuelExpensesLink;
-    @FindBy(xpath = ".//a[contains(@href, '/instructions')]")
-    private WebElement instructionsLink;
+public class UserSidebar extends Sidebar {
     @FindBy(xpath = ".//a[contains(@href, '/profile')]")
     private WebElement profileLink;
     @FindBy(xpath = ".//a[contains(@href, '/settings')]")
     private WebElement settingsLink;
-    @FindBy(css = ".btn.btn-link.text-danger.btn-sidebar.sidebar_btn")
-    private WebElement logoutLink;
 
     public UserSidebar(WebElement element) {
+        super(element);
         PageFactory.initElements(element, this);
-    }
-    public GaragePage openGarage(){
-        clickLink(garageLink);
-        return new GaragePage();
     }
     public ProfilePage openProfile(){
         clickLink(profileLink);
@@ -38,17 +27,5 @@ public class UserSidebar extends BasePage {
     public SettingsPage openSettings(){
         clickLink(settingsLink);
         return new SettingsPage();
-    }
-    public InstructionsPage openInstructions(){
-        clickLink(instructionsLink);
-        return new InstructionsPage();
-    }
-    public ExpensesPage openExpenses(){
-        clickLink(fuelExpensesLink);
-        return new ExpensesPage();
-    }
-    public MainPage logout(){
-        clickLink(logoutLink);
-        return new MainPage();
     }
 }
